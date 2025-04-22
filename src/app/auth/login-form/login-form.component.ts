246c533421c5a26922a94b47e524ced5063c941f
pierre1907 <saint-pierre.kassi@ism.edu.sn>
2025-04-22 23:07:17 +0000

    update login part

--- src/app/auth/login-form/login-form.component.ts
+++ src/app/auth/login-form/login-form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {CommonModule} from "@angular/common";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-form',
@@ -12,7 +13,10 @@ import {CommonModule} from "@angular/common";
 export class LoginFormComponent {
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
@@ -22,7 +26,21 @@ export class LoginFormComponent {
   onSubmit(): void {
     if (this.loginForm.valid) {
       console.log('Connexion avec :', this.loginForm.value);
-      // Redirection ou appel backend ici
+
+      const { email, password } = this.loginForm.value;
+
+      if (email === 'kassi@aloya.com' && password === 'admin123') {
+        this.loginForm.reset();
+        this.router.navigate(['/']);
+        /*this.notifierService.notify({
+          message: 'Connexion réussie !',
+          type: 'success'
+        });
+        */
+        // redirection ici
+      } else {
+
+      }
     }
   }
 }
