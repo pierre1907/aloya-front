5572d93c1ca95ba74462625b6b0ad3138bd28a94
pierre1907 <saint-pierre.kassi@ism.edu.sn>
2025-04-23 20:47:30 +0000

    add header with dropdown

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {LogOut, LucideAngularModule, Settings} from "lucide-angular";
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  dropdownOpen = false;
+
+  /* Icons lucide */
+  readonly parametres = Settings;
+  readonly logout = LogOut;
+
+  constructor(private eRef: ElementRef) {}
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent) {
+    // Ferme le dropdown si on clique en dehors
+    if (!this.eRef.nativeElement.contains(event.target)) {
+      this.dropdownOpen = false;
+    }
+  }
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  protected readonly Settings = Settings;
 }
